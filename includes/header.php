bd55e6a3758bc2d93f2e8c589624c504bd9155fe
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-11-10 10:54:47 +0100

    Better header

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,36 @@
 <!-- HEADER TO INCLUDE IN ALL PAGES -->
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <form class="navbar-form navbar-left" role="search">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Zoek...">
-      </div>
-      <button type="submit" class="btn btn-default">Zoek</button>
-    </form>
-    <div class="navbar-right">
-      <p class="navbar-text">Ingelogd als Admin</p>
-      <button type="button" class="btn btn-default navbar-btn">Log uit</button>
+    <div class="navbar-header">
+      <!-- Correct display on mobile screens -->
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
     </div>
-  </div>
+
+    <!-- Collection of all navbar elements to collapse on mobile -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+       <!-- Links -->
+      <ul class="nav navbar-nav">
+        <li><a href="#">Home</a></li>
+      </ul>
+
+      <!-- Search -->
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="NLT H3">
+        </div>
+        <button type="submit" class="btn btn-default">Zoek</button>
+      </form>
+
+       <!-- User panel -->
+      <ul class="nav navbar-nav navbar-right">
+        <li><p class="navbar-text">Ingelogd als Admin</p></li>
+        <li><button type="button" class="btn btn-default navbar-btn">Log uit</button></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div> <!-- /.container-fluid -->
 </nav>
